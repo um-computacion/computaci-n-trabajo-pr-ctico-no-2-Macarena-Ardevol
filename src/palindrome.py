c9660f7a7e1c717429197b57e2725b0cde37146f
Macarena <m.ardevol@alumno.um.edu.ar>
2025-04-15 12:10:15 -0300

    Implementación de la comparación de carcateres

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,3 +1,6 @@
 def is_palindrome(text):
     clean_text = clean_text_function(text)
     return clean_text == clean_text[::-1]
+
+def comparar_caracteres(texto_limpio):
+    return texto_limpio == texto_limpio[::-1]
